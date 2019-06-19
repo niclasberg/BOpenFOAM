9e585c8f7e0c5845c3a51d0994ca59146e875177
niclasberg <niclasbrg@gmail.com>
2019-06-19 17:21:54 +0200

    Added (optional) underrelaxation to the Windkessel boundary condition

--- OpenFOAM-5.x/src/windkessel/windkesselPressureFvPatchScalarField/WindkesselPressureFvPatchScalarField.C
+++ OpenFOAM-5.x/src/windkessel/windkesselPressureFvPatchScalarField/WindkesselPressureFvPatchScalarField.C
@@ -15,7 +15,8 @@ Foam::WindkesselPressureFvPatchScalarField::WindkesselPressureFvPatchScalarField
     L_(0),
     C_(0),
     Rp_(1),
-    Rc_(0)
+    Rc_(0),
+    imposeMean_(false)
 {
     // Set intial condition
     ps_.shiftAndInsert(0);
@@ -35,7 +36,8 @@ Foam::WindkesselPressureFvPatchScalarField::WindkesselPressureFvPatchScalarField
     L_(dict.lookupOrDefault<scalar>("L", 0)),
     C_(dict.lookupOrDefault<scalar>("C", 0)),
     Rp_(dict.lookupOrDefault<scalar>("Rp", 1)),
-    Rc_(dict.lookupOrDefault<scalar>("Rc", 0))
+    Rc_(dict.lookupOrDefault<scalar>("Rc", 0)),
+    imposeMean_(dict.lookupOrDefault<bool>("imposeMean", false))
 {
     // Evaluate initial condition if not present
     if(ps_.size() == 0) {
@@ -63,7 +65,8 @@ Foam::WindkesselPressureFvPatchScalarField::WindkesselPressureFvPatchScalarField
     L_(rhs.L_),
     C_(rhs.C_),
     Rp_(rhs.Rp_),
-    Rc_(rhs.Rc_)
+    Rc_(rhs.Rc_),
+    imposeMean_(rhs.imposeMean_)
 {
 
 }
@@ -81,7 +84,8 @@ Foam::WindkesselPressureFvPatchScalarField::WindkesselPressureFvPatchScalarField
     L_(rhs.L_),
     C_(rhs.C_),
     Rp_(rhs.Rp_),
-    Rc_(rhs.Rc_)
+    Rc_(rhs.Rc_),
+    imposeMean_(rhs.imposeMean_)
 {
 
 }
@@ -99,13 +103,23 @@ Foam::WindkesselPressureFvPatchScalarField::WindkesselPressureFvPatchScalarField
     L_(rhs.L_),
     C_(rhs.C_),
     Rp_(rhs.Rp_),
-    Rc_(rhs.Rc_)
+    Rc_(rhs.Rc_),
+    imposeMean_(rhs.imposeMean_)
 {
 
 }
 
 void Foam::WindkesselPressureFvPatchScalarField::updateCoeffs()
 {
+    // Read solver options
+    const dictionary& solverOptions = this->internalField().mesh().solverDict
+    (
+        "windkessel"
+    );
+
+    scalar relaxationFactor(solverOptions.lookupOrDefault("relaxationFactor", scalar(1)));
+    bool verbose(solverOptions.lookupOrDefault("verbose", false));
+
     // The ODE has the form
     //  a0 dp/dt + p = a1 Q + a2 dQ/dt + a3 d2Q/dt2
     // Evaluate coefficients
@@ -113,7 +127,7 @@ void Foam::WindkesselPressureFvPatchScalarField::updateCoeffs()
     scalar a1 = Rc_ + Rp_;
     scalar a2 = C_ * Rc_ * Rp_;
     scalar a3 = L_ * C_ * Rp_;
-
+    
     // Compute old flow rate if not yet computed
     if(qs_.size() == 0) {
         qs_.shiftAndInsert(this->computePatchFlowRate(this->U().oldTime()));
@@ -122,9 +136,11 @@ void Foam::WindkesselPressureFvPatchScalarField::updateCoeffs()
     // Check if we're on a new time step
     label timeIndex = this->db().time().timeIndex();
     if(timeIndex != lastTimeIndex_) {
-        // Add dummy values for the flow rate and pressure (will be replaced later)
+        // Insert a dummy value for the flow rate (will be recomputed during each iteration)
         qs_.shiftAndInsert(0);
-        ps_.shiftAndInsert(0);
+
+        // Insert the last pressure value as initial condition for this timestep
+        ps_.shiftAndInsert(ps_.currentValue());
         lastTimeIndex_ = timeIndex;
     }
 
@@ -134,9 +150,22 @@ void Foam::WindkesselPressureFvPatchScalarField::updateCoeffs()
     // Get current timestep
     scalar dt = this->db().time().deltaTValue();
 
+    if(verbose)
+        Info << "Updating Windkessel pressure for patch " << this->patch().name() << endl;
+
+    scalar lastValue = ps_.currentValue();
+    scalar newValue = 0;
     if(qs_.size() < 3) {
         // First timestep, integrate pressure with implicit Euler
-        ps_.currentValue() = (a0*ps_.oldValue() + a1*dt*qs_.currentValue() + a2*(qs_.currentValue() - qs_.oldValue())) / (a0 + dt);
+        newValue = (a0*ps_.oldValue() + a1*dt*qs_.currentValue() + a2*(qs_.currentValue() - qs_.oldValue())) / (a0 + dt);
+
+        if(verbose) {
+            Info << " Rp*C*dp/dt = " << a0 * (newValue - ps_.oldValue()) / dt 
+                << ", p = " << ps_.currentValue()
+                << ", (Rc+Rp)*Q = " << a1 * qs_.currentValue() 
+                << ", C*Rp*Rc*dQ/dt = " << a2 * (qs_.currentValue() - qs_.oldValue()) / dt
+                << ", L*C*Rp*d2Q/dt2 = " << 0 << endl;
+        }
     } else {
         // Get old timestep
         scalar dt0 = this->db().time().deltaT0Value();
@@ -156,20 +185,49 @@ void Foam::WindkesselPressureFvPatchScalarField::updateCoeffs()
         scalar dd = 2. / (dt * (dt + dt0));
 
         // Integrate the equation for the pressure
-        ps_.currentValue() = (
+        newValue = (
                     -a0*d0*ps_.oldValue() - a0*d00*ps_.oldOldValue() + 
                     (a1 + a2*d + a3*dd)*qs_.currentValue() + 
                     (a2*d0+a3*dd0)*qs_.oldValue() + 
                     (a2*d00 + a3*dd00)*qs_.oldOldValue()
             ) / (1 + a0*d);
+
+        if(verbose) {
+            Info << " Rp*C*dp/dt = " << a0 * (d*newValue + d0*ps_.oldValue() + d00*ps_.oldOldValue())
+                << ", p = " << ps_.currentValue()
+                << ", (Rc+Rp)*Q = " << a1 * qs_.currentValue() 
+                << ", C*Rp*Rc*dQ/dt = " << a2 * (d*qs_.currentValue() + d0*qs_.oldValue() + d00*qs_.oldOldValue())
+                << ", L*C*Rp*d2Q/dt2 = " << a3 * (dd*qs_.currentValue() + dd0*qs_.oldValue() + dd00*qs_.oldOldValue() ) << endl;
+        }
     }
 
-    //ps_.write(Info);
-    //qs_.write(Info);
+    // Evaluate the new (underrelaxed) value
+    ps_.currentValue() = newValue*relaxationFactor + (1. - relaxationFactor)*lastValue;
+    if(verbose) {
+        Info << " Old value = " << lastValue << ", new value = " << ps_.currentValue() << ", delta = " << (ps_.currentValue() - lastValue) << endl;
+    }
 
     // Apply values to the faces
-    operator==(ps_.currentValue());
+    if(imposeMean_) {
+        scalarField newValues(this->patchInternalField());
+        scalar meanValuePsi = gSum(this->patch().magSf()*newValues)
+                                /gSum(this->patch().magSf());
+        scalar meanValue = ps_.currentValue();
 
+        if (mag(meanValue) > SMALL && mag(meanValuePsi)/mag(meanValue) > 0.5)
+        {
+            newValues *= mag(meanValue)/mag(meanValuePsi);
+        }
+        else
+        {
+            newValues += (meanValue - meanValuePsi);
+        }
+
+        this->operator==(newValues);
+    } else {
+        this->operator==(ps_.currentValue());
+    }
+    
     fixedValueFvPatchField<scalar>::updateCoeffs();
 }
 
@@ -189,6 +247,7 @@ void Foam::WindkesselPressureFvPatchScalarField::write(Foam::Ostream & os) const
     this->writeEntryIfDifferent(os, "Rc", scalar(0), Rc_);
     this->writeEntryIfDifferent(os, "L", scalar(0), L_);
     this->writeEntryIfDifferent(os, "C", scalar(0), C_);
+    this->writeEntryIfDifferent(os, "imposeMean", false, imposeMean_);
     qs_.write(os);
     ps_.write(os);
     this->writeEntry("value", os);
